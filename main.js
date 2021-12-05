4075e1bc3d1bfe0dc66ebc49179338fd04820bf5
KoJaem <woalswhwh@naver.com>
2021-12-05 22:10:43 +0900

    기초 틀 잡아둠 (부트스트랩, chartJs 사용)

---
+++ main.js
@@ -0,0 +1,22 @@
+const gender = document.querySelector("#gender");
+const height = document.querySelector("#height");
+const weight = document.querySelector("#weight");
+const age = document.querySelector("#age");
+const user = { }
+const okButton = document.querySelector("#okBtn");
+okButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    // input 창에 입력되지 않은 것이 있을때
+    if(!gender.value || !height.value || !weight.value || !age.value)
+    {
+        alert("모든 정보를 입력해주세요.");
+        return;
+    }
+    user.gender = gender.value;
+    user.height = Number(height.value);
+    user.weight = Number(weight.value);
+    user.age = Number(age.value);
+    user.bmi = Math.round((user.weight/((user.height/100)**2))*100)/100;
+    console.log("사용자님의 BMI 수치는",user.bmi);
+});
+
---
+++ main2.js
@@ -0,0 +1,37 @@
+const progress = document.querySelector("#progress");
+// progress.style.width = "10%";
+
+      var ctx = document.getElementById("myChart").getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: ["저체중", "정상", "과체중", "비만", "고도비만"],
+          datasets: [
+            {
+              data: [90, 31, 29, 25, 29],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          plugins: {
+            legend: {
+              position : "right"
+            }
+          }
+        }
+      })
